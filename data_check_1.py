e189660bf1362135cfed67e8d9d63d3c1a413ac5
kyhokie <kyhokie@gmail.com>
2022-09-29 13:28:34 -0400

    Completed Knowledge Check 1

--- data_check_1.py
+++ data_check_1.py
@@ -1,4 +1,14 @@
+#statement that prints "hello world"
 print("hello world")
 
+#list populated with several values ... print a value
 months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
 print(months [1])
+
+#dictionary populated with two keys and two values.  Print one value.
+triple_crown_winners = {"Secretariat" : "1973", "Citation" : "1948", "Affirmed" : "1978", "Justify" : "2018"}
+print(triple_crown_winners["Affirmed"])
+
+#tuple with 4 values. Print one of them.
+famous_race_horses = ("Man o'War", "Seabiscuit", "Sir Barton", "John Henry")
+print(famous_race_horses[0])
